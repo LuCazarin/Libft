19c0a8aa9aaf85375ed4434ba0436b2774e091f7
LuCazarin <cazarinluana13@gmail.com>
2026-06-03 22:45:28 -0300

    Adiciona ft_calloc

---
+++ ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/21 10:26:44 by luafranc          #+#    #+#             */
+/*   Updated: 2026/06/03 22:34:28 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void  *ft_calloc(size_t n, size_t size)
+{
+  void  *ptr;
+
+  if (n  != 0 && (n * size) / n != size)
+    return (NULL);
+  ptr = malloc(n * size);
+  if (!ptr)
+    return (NULL);
+  ft_bzero(ptr, n * size);
+  return (ptr);
+}
